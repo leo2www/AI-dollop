31f23a3892135ddf867761de8c687b999efd3132
hazy <hazy@hazy.com>
2024-01-13 20:29:12 +0800

    添加垂直布局Column和按键Button。Button点击行动用TODO占位，文字调用资源文件里的roll。资源文件string.xml，可使用Translations Editor 修改，只是添加key-value而已。

--- app/src/main/java/com/hazy/diceroller/MainActivity.kt
+++ app/src/main/java/com/hazy/diceroller/MainActivity.kt
@@ -3,11 +3,15 @@ package com.hazy.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.hazy.diceroller.ui.theme.DiceRollerTheme
 
@@ -26,7 +30,16 @@ class MainActivity : ComponentActivity() {
 // 默认Modifier对象
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-
+//    创建Column界面
+    Column (
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally  //垂直主轴对齐，居中
+    ){  // 下面是一个lambda表达式，采用尾随语法
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+            // roll字符串的if传到stringResource()函数，结果到Text可组合项
+        }
+    }
 }
 
 @Preview
