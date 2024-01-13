a8550c09fa399ada7ab56261886307c30ea9fb6b
hazy <hazy@hazy.com>
2024-01-13 20:39:34 +0800

    添加图片

--- app/src/main/java/com/hazy/diceroller/MainActivity.kt
+++ app/src/main/java/com/hazy/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hazy.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.hazy.diceroller.ui.theme.DiceRollerTheme
@@ -35,6 +37,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally  //垂直主轴对齐，居中
     ){  // 下面是一个lambda表达式，采用尾随语法
+        Image(
+            painter = painterResource(id = R.drawable.dice_1), // 图片
+            contentDescription = "1" // 内容说明
+        )
+
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
             // roll字符串的if传到stringResource()函数，结果到Text可组合项
